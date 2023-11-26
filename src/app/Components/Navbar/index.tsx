d0780bc41529270ae30ce4f566e57aa9857efd38
yogga18 <yogga910@gmail.com>
2023-11-26 07:40:06 +0700

    adding icons

--- src/app/Components/Navbar/index.tsx
+++ src/app/Components/Navbar/index.tsx
@@ -2,46 +2,72 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { FaPhoneVolume } from 'react-icons/fa6';
+import { FaHome, FaBookReader } from 'react-icons/fa';
+import { GrProjects, GrDocumentPdf } from 'react-icons/gr';
 
 const NavigationBar = () => {
+  const dataPath = [
+    {
+      id: 1,
+      name: 'About',
+      path: '/',
+      icon: <FaHome />,
+    },
+    {
+      id: 2,
+      name: 'Project',
+      path: '/project',
+      icon: <GrProjects />,
+    },
+    {
+      id: 3,
+      name: 'Blog',
+      path: '/blog',
+      icon: <FaBookReader />,
+    },
+    {
+      id: 4,
+      name: 'Contact',
+      path: '/contact',
+      icon: <FaPhoneVolume />,
+    },
+    {
+      id: 5,
+      name: 'CV',
+      path: '/cv',
+      icon: <GrDocumentPdf />,
+    },
+  ];
+
   const pathname = usePathname();
+
   return (
-    <ul className='flex flex-row md:flex-col gap-3 bg-blue-300 h-full w-full align-baseline justify-center items-center border-white border-t-2 md:border-2'>
-      <li
-        className={`font-thin md:font-bold text-white font-sans ${
-          pathname === '/' ? 'border border-white p-2 rounded-lg' : ''
-        }`}
-      >
-        <Link href='/'>About</Link>
-      </li>
-      <li
-        className={`font-thin md:font-bold text-white font-sans ${
-          pathname === '/project' ? 'border border-white p-2 rounded-lg' : ''
-        }`}
-      >
-        <Link href='/project'>Project</Link>
-      </li>
-      <li
-        className={`font-thin md:font-bold text-white font-sans ${
-          pathname === '/blog' ? 'border border-white p-2 rounded-lg' : ''
-        }`}
-      >
-        <Link href='/blog'>Blog</Link>
-      </li>
-      <li
-        className={`font-thin md:font-bold text-white font-sans ${
-          pathname === '/contact' ? 'border border-white p-2 rounded-lg' : ''
-        }`}
-      >
-        <Link href='/contact'>Contact</Link>
-      </li>
-      <li
-        className={`font-thin md:font-bold text-white font-sans ${
-          pathname === '/cv' ? 'border border-white p-2 rounded-lg' : ''
-        }`}
-      >
-        <Link href='/cv'>CV</Link>
-      </li>
+    <ul className='flex flex-row md:flex-col gap-6 bg-blue-300 h-full w-full align-baseline justify-center items-center border-white border-t-2 md:border-2'>
+      {dataPath.map((item) => {
+        return (
+          <li
+            key={item.id}
+            className={`hidden md:block font-bold text-white font-sans ${
+              pathname === item.path ? 'border border-white p-2 rounded-lg' : ''
+            }`}
+          >
+            <Link href={item.path}>{item.name}</Link>
+          </li>
+        );
+      })}
+      {dataPath.map((item) => {
+        return (
+          <li
+            key={item.id}
+            className={`block md:hidden text-white text-2xl font-sans ${
+              pathname === item.path ? 'border border-white p-2 rounded-lg' : ''
+            }`}
+          >
+            <Link href={item.path}>{item.icon}</Link>
+          </li>
+        );
+      })}
     </ul>
   );
 };
