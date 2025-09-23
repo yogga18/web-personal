735409c8cd73590b7427164162727e29d6db6fc7
Yogga Aditya Candra <yogga910@gmail.com>
2025-09-23 13:37:37 +0700

    add key at blog page

--- src/app/(pages)/blog/page.tsx
+++ src/app/(pages)/blog/page.tsx
@@ -1,20 +1,27 @@
-import type { Metadata } from 'next';
-import { DummyDataBlog } from '../../DummyData/DimmyDataBlog';
-import PrimaryCard from '@/app/Components/Card/PrimaryCard';
+import type { Metadata } from "next";
+import { DummyDataBlog } from "../../DummyData/DimmyDataBlog";
+import PrimaryCard from "@/app/Components/Card/PrimaryCard";
 
 export const metadata: Metadata = {
-  title: 'Read My Blog',
-  description: 'Blog Page',
+  title: "Read My Blog",
+  description: "Blog Page",
 };
 
 const Blog = () => {
   const DataDummy = DummyDataBlog.sort((a, b) => b.id - a.id);
 
   return (
-    <div className='grid grid-cols-12 w-full h-screen overflow-y-scroll'>
-      <div className='col-span-12 mt-10'>
+    <div className="grid grid-cols-12 w-full h-screen overflow-y-scroll">
+      <div className="col-span-12 mt-10">
         {DataDummy.map((data) => {
-          return <PrimaryCard key={data.id} data={data} isMetaData={true} />;
+          return (
+            <PrimaryCard
+              key={data.id}
+              id={data.id}
+              data={data}
+              isMetaData={true}
+            />
+          );
         })}
       </div>
     </div>
