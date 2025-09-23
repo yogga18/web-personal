e82570d31119d328e4f789cc77007fb66c3a1e77
Yogga Aditya Candra <yogga910@gmail.com>
2025-09-23 19:49:23 +0700

    fix : url blog

--- src/app/DummyData/DimmyDataBlog.js
+++ src/app/DummyData/DimmyDataBlog.js
@@ -2,90 +2,90 @@ export const DummyDataBlog = [
   {
     id: 1,
     thumbnail:
-      'https://miro.medium.com/v2/resize:fit:828/format:webp/0*vWlJRlYoPlkjCtfF',
-    url: 'https://medium.com/@yoggacandra/01-reactjs-basic-fetching-api-cda72c94912c',
-    title: 'Reactjs Basic :) fetching Api',
-    createdAt: '20 April 2021',
-    author: 'Yoga Aditya Candra',
+      "https://miro.medium.com/v2/resize:fit:828/format:webp/0*vWlJRlYoPlkjCtfF",
+    url: "https://medium.com/@yoggacandra/01-reactjs-basic-fetching-api-cda72c94912c",
+    title: "Reactjs Basic :) fetching Api",
+    createdAt: "20 April 2021",
+    author: "Yoga Aditya Candra",
     description:
-      'Fetch atau Fetching data ke API ini adalah suatu kegiatan di mana kita meminta sumber daya kesuatu resources dan akan kita gunakan entah untuk di display di halama web atau lebih jauh lagi dari itu, kegiatan ini pula tidak jauh dari requset dan response.',
+      "Fetch atau Fetching data ke API ini adalah suatu kegiatan di mana kita meminta sumber daya kesuatu resources dan akan kita gunakan entah untuk di display di halama web atau lebih jauh lagi dari itu, kegiatan ini pula tidak jauh dari requset dan response.",
   },
   {
     id: 2,
     thumbnail:
-      'https://miro.medium.com/v2/resize:fit:828/format:webp/0*JnMDaOeyYS6wDMSa',
-    url: 'https://medium.com/@yoggacandra/02-fetching-api-async-await-menggunakan-react-hook-31c60bab77c7',
-    title: 'Fetching Api async await menggunakan React Hook',
-    createdAt: '24 April 2021',
-    author: 'Yoga Aditya Candra',
+      "https://miro.medium.com/v2/resize:fit:828/format:webp/0*JnMDaOeyYS6wDMSa",
+    url: "https://medium.com/@yoggacandra/02-fetching-api-async-await-menggunakan-react-hook-31c60bab77c7",
+    title: "Fetching Api async await menggunakan React Hook",
+    createdAt: "24 April 2021",
+    author: "Yoga Aditya Candra",
     description:
-      'Hai Freen… di artikel sebelumnya kita sudah membahas bagai mana Fetching Api menggunakan class component di artikel itu pula kita mengetahui bahwa Ketika Fetch (request) sebuah Api kita memiliki waktu tunggu yang di mana waktu tunggu itu adalah response dari API dari apa yang',
+      "Hai Freen… di artikel sebelumnya kita sudah membahas bagai mana Fetching Api menggunakan class component di artikel itu pula kita mengetahui bahwa Ketika Fetch (request) sebuah Api kita memiliki waktu tunggu yang di mana waktu tunggu itu adalah response dari API dari apa yang",
   },
   {
     id: 3,
     thumbnail:
-      'https://miro.medium.com/v2/resize:fit:828/format:webp/0*LtTswU_Pg_6yf8Lt',
-    url: 'https://medium.com/@yoggacandra/mari-kita-bahas-sedikit-tentang-css-specificity-5ca4312a98bc',
-    title: 'Mari kita bahas sedikit tentang CSS Specificity',
-    createdAt: '29 May 2021',
-    author: 'Yoga Aditya Candra',
+      "https://miro.medium.com/v2/resize:fit:828/format:webp/0*LtTswU_Pg_6yf8Lt",
+    url: "https://medium.com/@yoggacandra/mari-kita-bahas-sedikit-tentang-css-specificity-5ca4312a98bc",
+    title: "Mari kita bahas sedikit tentang CSS Specificity",
+    createdAt: "29 May 2021",
+    author: "Yoga Aditya Candra",
     description:
-      'Cascading Style Sheet atau biasa disingkat CSS ini berguna untuk mengatur halaman front end web lebih tepatnya CSS akan memberikan styling pada HTML, sejujurnya bermain dengan CSS adalah salah satu hal yang sangat menyenangkan bagi saya bermain dengan tempilan dan memberikan mereka styling yang berbeda — beda. Karena terlalu lama',
+      "Cascading Style Sheet atau biasa disingkat CSS ini berguna untuk mengatur halaman front end web lebih tepatnya CSS akan memberikan styling pada HTML, sejujurnya bermain dengan CSS adalah salah satu hal yang sangat menyenangkan bagi saya bermain dengan tempilan dan memberikan mereka styling yang berbeda — beda. Karena terlalu lama",
   },
   {
     id: 4,
     thumbnail:
-      'https://miro.medium.com/v2/resize:fit:828/format:webp/0*VIKMkqPnHsPn4GVA',
-    url: 'https://medium.com/@yoggacandra/13-shortcut-dasar-vscode-yang-perlu-anda-ingat-3d64478c9151',
-    title: '13 Shortcut dasar VSCode yang perlu anda ingat',
-    createdAt: '4 Juni 2021',
-    author: 'Yogga Aditya Candra',
+      "https://miro.medium.com/v2/resize:fit:828/format:webp/0*VIKMkqPnHsPn4GVA",
+    url: "https://medium.com/@yoggacandra/13-shortcut-dasar-vscode-yang-perlu-anda-ingat-3d64478c9151",
+    title: "13 Shortcut dasar VSCode yang perlu anda ingat",
+    createdAt: "4 Juni 2021",
+    author: "Yogga Aditya Candra",
     description:
-      'Lupa - lupa ingat yup itu ungkapan yang tepat untuk Shortcut dari saya padahal shortcut di VSCode sangat membantu untuk menunjang produksitivas sang penggunanya agar ngoding lebih k̶e̶r̶e̶n̶ cepat karena kita akan terfokuskan kepada screen dan keyboard tidak perlu repot geser - geser mouse, s̶u̶d̶a̶h̶i̶ ̶i̶n̶t̶r̶o̶n̶y̶a̶ mari kita masuk dalam pembahasan.',
+      "Lupa - lupa ingat yup itu ungkapan yang tepat untuk Shortcut dari saya padahal shortcut di VSCode sangat membantu untuk menunjang produksitivas sang penggunanya agar ngoding lebih k̶e̶r̶e̶n̶ cepat karena kita akan terfokuskan kepada screen dan keyboard tidak perlu repot geser - geser mouse, s̶u̶d̶a̶h̶i̶ ̶i̶n̶t̶r̶o̶n̶y̶a̶ mari kita masuk dalam pembahasan.",
   },
   {
     id: 5,
     thumbnail:
-      'https://miro.medium.com/v2/resize:fit:828/format:webp/0*VLJlkqhf572l5uzs',
-    url: 'https://medium.com/@yoggacandra/javascript-loop-e7315f4d823a',
-    title: 'Javascript Loop',
-    createdAt: '10 Maret 2021',
-    author: 'Yogga Aditya Candra',
+      "https://miro.medium.com/v2/resize:fit:828/format:webp/0*VLJlkqhf572l5uzs",
+    url: "https://medium.com/@yoggacandra/javascript-loop-e7315f4d823a",
+    title: "Javascript Loop",
+    createdAt: "10 Maret 2021",
+    author: "Yogga Aditya Candra",
     description:
-      'For Loop For akan melakukan perulanagan dalam blok kode di dalam scopenya ia akan berhenti ketika kondisinya selesai atau sudah tidak terpenuhi lagi. // init statement ( optional ) condition ( optional ) increment / decrement statement ( optional ) for(initial value statement; kondisi; post statement){ Block perulangan}',
+      "For Loop For akan melakukan perulanagan dalam blok kode di dalam scopenya ia akan berhenti ketika kondisinya selesai atau sudah tidak terpenuhi lagi. // init statement ( optional ) condition ( optional ) increment / decrement statement ( optional ) for(initial value statement; kondisi; post statement){ Block perulangan}",
   },
   {
     id: 6,
     thumbnail:
-      'https://miro.medium.com/v2/resize:fit:828/format:webp/0*NP8gkucMajpQ7e0v',
-    url: 'https://medium.com/@yoggacandra/mengenal-function-pada-javascript-7e8d879acd4',
-    title: 'Mengenal Function pada Javascript',
-    createdAt: '29 Maret 2021',
-    author: 'Yogga Aditya Candra',
+      "https://miro.medium.com/v2/resize:fit:828/format:webp/0*NP8gkucMajpQ7e0v",
+    url: "https://medium.com/@yoggacandra/mengenal-function-pada-javascript-7e8d879acd4",
+    title: "Mengenal Function pada Javascript",
+    createdAt: "29 Maret 2021",
+    author: "Yogga Aditya Candra",
     description:
-      'Agar terhindar dari penulisan kode yang berulang salah satu caranya kita dapat menggunakan Function, function itu sendiri adalah sebuah kode pemrograman yang dibuat untuk menjalankan tugas tertentu, function pada javascript biasanya ( tidak harus ) dibuat dengan nama camel case sebagai contoh printText(){}. Berikut ini beberpa hal yang perlu anda',
+      "Agar terhindar dari penulisan kode yang berulang salah satu caranya kita dapat menggunakan Function, function itu sendiri adalah sebuah kode pemrograman yang dibuat untuk menjalankan tugas tertentu, function pada javascript biasanya ( tidak harus ) dibuat dengan nama camel case sebagai contoh printText(){}. Berikut ini beberpa hal yang perlu anda",
   },
   {
     id: 7,
     thumbnail:
-      'https://miro.medium.com/v2/resize:fit:828/format:webp/0*UGD30au6h7KsJCwd',
-    url: 'https://medium.com/@yoggacandra/hands-on-docker-c4a1e4769246',
-    title: 'Hands on DOCKER 🐳',
-    createdAt: '29 February 2024',
-    author: 'Yogga Aditya Candra',
+      "https://miro.medium.com/v2/resize:fit:828/format:webp/0*UGD30au6h7KsJCwd",
+    url: "https://medium.com/@yoggacandra/hands-on-docker-c4a1e4769246",
+    title: "Hands on DOCKER 🐳",
+    createdAt: "29 February 2024",
+    author: "Yogga Aditya Candra",
     description:
-      'For example we can install ubuntu from locally, but if ubuntu not found we can install ubuntu from docker hub let’s me show you…',
+      "For example we can install ubuntu from locally, but if ubuntu not found we can install ubuntu from docker hub let’s me show you…",
   },
   {
     id: 8,
     thumbnail:
-      'https://miro.medium.com/v2/resize:fit:1400/format:webp/0*hAO9WmrBjJGK2HTH',
-    url: 'https://miro.medium.com/v2/resize:fit:828/format:webp/0*hAO9WmrBjJGK2HTH',
+      "https://miro.medium.com/v2/resize:fit:1400/format:webp/0*hAO9WmrBjJGK2HTH",
+    url: "https://medium.com/@yoggacandra/first-step-with-next-js-14-embark-on-your-adventure-with-nextjs-932ccb4f6018",
     title:
-      'First Step with Next.js 14: Embark on Your Adventure with NextJs 🔥🔥',
-    createdAt: '2 Maret 2024',
-    author: 'Yogga Aditya Candra',
+      "First Step with Next.js 14: Embark on Your Adventure with NextJs 🔥🔥",
+    createdAt: "2 Maret 2024",
+    author: "Yogga Aditya Candra",
     description:
-      'Hey there! 😎 Just wanted to drop you a line and let you know I just wrapped up writing about Next.js 14. Yeah, I know, it’s pretty basic stuff, but hey, if you’ve got a few minutes to spare, maybe you could give it a read. I’d love to hear your thoughts on it!',
+      "Hey there! 😎 Just wanted to drop you a line and let you know I just wrapped up writing about Next.js 14. Yeah, I know, it’s pretty basic stuff, but hey, if you’ve got a few minutes to spare, maybe you could give it a read. I’d love to hear your thoughts on it!",
   },
 ];
